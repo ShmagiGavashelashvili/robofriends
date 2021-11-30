6c6966a500d4bf40a1bbc8f6755eca97a48c3220
shmagi-avro <shmagigavashelashvili@yahoo.com>
2021-11-30 13:39:51 +0400

    ffixed sum vulerabilities

--- src/Components/RoboFriendCard.js
+++ src/Components/RoboFriendCard.js
@@ -3,7 +3,7 @@ import React from "react";
 function RoboFriendCard({ id, username, name, email, random }) {
   return (
     <div className="robofriends_card">
-      <img src={`https://robohash.org/${random}${id}`} alt={username} />
+      <img src={`https://robohash.org/${random}${id}?size=200x200`} alt={username} />
       <h2>{name}</h2>
       <p>{email}</p>
     </div>
