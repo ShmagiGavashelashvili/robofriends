148da7ebcd6f1d2452458f9bfa9e98b58dc549bb
shmagi-avro <shmagigavashelashvili@yahoo.com>
2021-12-19 12:13:23 +0400

    securiti issues fixzed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import RoboFriends from "./Components/RoboFriends";
 import React, { memo } from "react";
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <RoboFriends />
